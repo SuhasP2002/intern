1b4e2edab6145148a42e5d8e7cb3830bc9e66193
Suhas Santosh Pai <suhaspai412@gmail.com>
2023-03-14 14:54:30 +0530

    add

---
+++ main.js
@@ -0,0 +1,34 @@
+let value = 0;
+changeColor = () => {
+  if (value <0)
+  {
+    document.querySelector("#value").style.color = "#E21717";
+
+  }else if(value > 0){
+    document.querySelector("#value").style.color = "#F7CD2E";
+  }else{
+    document.querySelector("#value").style.color = "#fff";
+
+  }
+}
+
+document.querySelector(".increase").onclick = () => {
+  value = value + 1;
+  changeColor();
+  document.querySelector("#value").innerHTML = value;
+
+}
+
+document.querySelector(".decrease").onclick = () => {
+  value = value - 1;
+  changeColor();
+  document.querySelector("#value").innerHTML = value;
+
+}
+
+document.querySelector(".reset").onclick = () => {
+  value = 0;
+  changeColor();
+  document.querySelector("#value").innerHTML = value;
+   
+}
